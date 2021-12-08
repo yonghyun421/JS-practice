0c9f6ef3c37daf2f90d0826d9a27842d07dc9c87
yonghyun <dydgus0421@gmail.com>
2021-12-08 17:28:28 +0900

    next-js 초기세팅 study-1

---
+++ src/component/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <div>Copyright 어쩌구저쩌구</div>;
+}
---
+++ src/component/Top.js
@@ -0,0 +1,9 @@
+import { Header } from 'semantic-ui-react';
+
+export default function Top() {
+    return <div>
+        <img src='/images/ts.png' alt='logo' />
+        <Header as='h1'>투데위</Header>
+        로고, 텍스트, GNB
+        </div>;
+}
