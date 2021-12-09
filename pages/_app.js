8078fcd3921f6ac446e2b11626cae5286569fdc5
yonghyun <dydgus0421@gmail.com>
2021-12-09 16:55:54 +0900

    초기세팅 study-2

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Top from '../src/component/Top'
 
 function MyApp({ Component, pageProps }) {
   return( 
-  <div>
+  <div style={{ width: 1000, margin: '0 auto' }}>
     <Top />
     <Component {...pageProps} />
     <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
+import { Axios } from 'axios';
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [list, setList] = useState([]);
+  const API_URL = 
+  'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+
+  function getData() {
+    Axios.get(API_URL)
+    .then(res => {
+      console.log(res)
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
-    <div>create-next-app</div>
+    <div>
+      <Head>
+        <title>HOME | 투데위</title>
+      </Head>
+      create-next-app
+    </div>
   )
 }
